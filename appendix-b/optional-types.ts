1ec1cdbbe9d713f42928b03ba4369fd4519559ca
DercilioFontes <derciliofontes@gmail.com>
2019-06-24 20:59:27 -0700

    Optional Types

---
+++ appendix-b/optional-types.ts
@@ -0,0 +1,16 @@
+let name1 = 'John Smith';
+
+let name2: string = 'John Smith';
+
+let salary: number;
+let isValid: boolean;
+let customerName: string = null;
+
+let name3: any = 'John Smith';
+name3 = 123;
+
+let element: HTMLElement;
+let doc: Document;
+
+console.log(name1);
+console.log(name2);
