878ceeb04e7a730c509fe0e16b524b33f5da2a28
DercilioFontes <derciliofontes@gmail.com>
2019-06-26 00:10:18 -0700

    function with question mark

--- functions/calculating-tax.ts
+++ functions/calculating-tax.ts
@@ -1,10 +1,25 @@
-function calcTax(state: string = 'NY', income: number, dependents: number): number {
+function calcTax(
+  income: number,
+  state: string = 'NY',
+  dependents?: number
+): number {
+  let deduction: number;
+
+  if (dependents) {
+    deduction = dependents * 500;
+  } else {
+    deduction = 0;
+  }
+
   if (state === 'NY') {
-    return income * 0.06 - dependents * 500;
+    return income * 0.06 - deduction;
   } else if (state === 'NJ') {
-    return income * 0.05 - dependents * 300;
+    return income * 0.05 - deduction;
   }
 }
 
-let tax = calcTax('NJ', 50000, 2);
-console.log(tax);
+let tax: number = calcTax(50000, 'NJ', 3);
+console.log(`Your tax is ${tax}`);
+
+tax = calcTax(50000);
+console.log(`Your tax is ${tax}`);
